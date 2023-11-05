6791bebf157dd3bbb4ae8a049d33e969101aeeb6
jaspinder@neurabytes.com <jaspinder@neurabytes.com>
2023-11-04 22:41:07 -0400

    Refactored code & Fix the code to identify the existing key

--- bin/GitHubGPGKey.ps1
+++ bin/GitHubGPGKey.ps1
@@ -1,13 +1,17 @@
-# Ensure GPG is installed
-if (-not (Get-Command "gpg" -ErrorAction SilentlyContinue)) {
-    Write-Error "GPG is not found on this system. Please install it first."
-    exit 1
+function Test-GPGInstallation {
+    if (-not (Get-Command "gpg" -ErrorAction SilentlyContinue)) {
+        Write-Error "GPG is not found on this system. Please install it first."
+        exit 1
+    }
 }
 
-function GenerateGPGKey($email, $name) {
-    $gpgVersion = (gpg --version | Select-Object -First 1).Split(" ")[2]
+function Get-GPGVersion {
+    return (gpg --version | Select-Object -First 1).Split(" ")[2]
+}
 
-    $gpgCmd = @"
+function New-GPGCommandFile($email, $name) {
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    @"
 Key-Type: RSA
 Key-Length: 4096
 Subkey-Type: RSA
@@ -16,10 +20,13 @@ Name-Real: $name
 Name-Email: $email
 Expire-Date: 0
 %commit
-"@
-    # Use a temporary file for GPG commands
-    $tempFile = [System.IO.Path]::GetTempFileName()
-    $gpgCmd | Out-File -Encoding ascii -FilePath $tempFile
+"@ | Out-File -Encoding ascii -FilePath $tempFile
+    return $tempFile
+}
+
+function Invoke-GPGKeyGeneration($email, $name) {
+    $gpgVersion = Get-GPGVersion
+    $tempFile = New-GPGCommandFile $email $name
 
     Write-Host "Generating GPG key. Please follow any prompts..."
     if ([Version]$gpgVersion -ge [Version]"2.1.17") {
@@ -28,71 +35,72 @@ Expire-Date: 0
         gpg --gen-key --batch $tempFile
     }
 
-    # Retrieve keyId for the given email address
-    $keyId = gpg --list-secret-keys --keyid-format LONG "$email" | Where-Object { $_ -match "sec\s+rsa4096/(\w+)" } | ForEach-Object { $matches[1] }
-
-    $publicKeyFile = "$env:USERPROFILE\public_key.asc"
-
-    gpg --export -a $keyId > $publicKeyFile
-
-    Write-Host "Public key saved to: $publicKeyFile"
-    Write-Host "Please save the key from this location and then delete the file."
-
-
     Remove-Item $tempFile -Force
-
-    # Return the keyId, email, and name as a custom object
-    return [PSCustomObject]@{
-        'KeyID' = $keyId
-        'Email' = $email
-        'Name'  = $name
-    }
 }
 
+function Get-KeyIDByEmail($email) {
+    return gpg --list-secret-keys --keyid-format LONG "$email" |
+            Where-Object { $_ -match "sec\s+rsa4096/(\w+)" } |
+            ForEach-Object { $matches[1] }
+}
+
+function Export-GPGPublicKey($keyId) {
+    $publicKeyFile = "$env:USERPROFILE\public_key.asc"
+    gpg --export -a $keyId > $publicKeyFile
+    Write-Host "Public key saved to: $publicKeyFile"
+    Write-Host "Please save the key from this location and then delete the file."
+}
+
+function Find-GPGKeyByEmail($email, $name) {
+    $escapedEmail = [regex]::Escape($email)
+    $existingKeys = gpg --list-keys --with-colons | Where-Object { $_ -like "*<${email}>*" }
+    $pattern = "::([A-F0-9]{40})::.*?<${escapedEmail}>"
+
+    foreach ($key in $existingKeys) {
+        if ($key -match $pattern) {
+            return @{
+                UID = $matches[1].Trim()
+                Email = $email
+                Name = $name
+            }
+        }
+    }
+
+    return $null
+}
+
+function Set-GitGPGConfiguration($keyId) {
+    $gpg_location = (Get-Command "gpg").Source
+    git config --replace-all --global gpg.program $gpg_location
+    git config --replace-all --global user.signingkey $keyId
+    git config --global commit.gpgsign true
+}
+
+# Main Script Logic
+Test-GPGInstallation
 
 $name = Read-Host "Enter your name (e.g., John Doe):"
 $email = Read-Host "Enter your verified email address for your GitHub account:"
 
-# Check for existing GPG keys with the provided email
-# TODO - This logic is not working and new keys are always generated
-$existingKeys = gpg --list-keys --with-colons | where { $_ -like "*<${email}>*" }
-
-$existingEntry = $null
-
-foreach ($key in $existingKeys) {
-    if ($key -match "uid:(.*?):<(${email})>") {
-        $existingEntry = @{
-            Name = $matches[1].Trim()
-            Email = $matches[2]
-        }
-        break
-    }
-}
-
-$newKey = $null
+$existingEntry = Find-GPGKeyByEmail $email $name
 
 if ($existingEntry) {
-    Write-Host "A GPG key associated with name: $($existingEntry.Name) and email: $($existingEntry.Email) exists."
+    Write-Host "A GPG key associated with Name: $($existingEntry.Name) and email: $($existingEntry.Email) exists. UID: $($existingEntry.UID)"
     $choice = Read-Host "Do you want to generate a new one? (Yes/No)"
-    if ($choice -eq "Yes") {
-        Write-Output "Generating a new GPG key, even when a key exists..."
-        $newKey = GenerateGPGKey $email $name
-    }
-    else
-    {
+    if ($choice -ne "Yes") {
         Write-Output "GPG key already exists. Exiting..."
+        exit
     }
-} else {
-    Write-Output "Key does not exists. Generating a new GPG key..."
-    $newKey = GenerateGPGKey $email $name
 }
 
-# Check if a key was generated successfully
-if ($null -ne $newKey.KeyID -and $newKey.KeyID -ne "") {
-    # The key was generated
-    $gpg_location = where.exe gpg
+Write-Output "Generating a new GPG key..."
+Invoke-GPGKeyGeneration $email $name
+$keyId = Get-KeyIDByEmail $email
 
-    git config --global gpg.program $gpg_location
-    git config --global user.signingkey $newKey.KeyID
-    git config --global commit.gpgsign true
+if ($keyId) {
+    Export-GPGPublicKey $keyId
+    Set-GitGPGConfiguration $keyId
+    Write-Output "GPG key has been generated and configured for Git."
+} else {
+    Write-Error "Failed to generate GPG key."
 }
